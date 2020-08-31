ea3513d6093bb7345fc60f7d3ac3db9515309a64
shabbyshen <447725977@qq.com>
2020-08-31 16:21:47 +0800

    配置alias别名

--- craco.config.js
+++ craco.config.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: Shabby申
+ * @Date: 2020-08-31 10:25:34
+ * @Last Modified by: Shabby申
+ * @Last Modified time: 2020-08-31 15:27:53
+ * 不弹射项目 修改webpack的默认配置 使用craco插件
+ */
 const CracoLessPlugin = require("craco-less");
 const path = require("path");
 const ProgressBarPlugin = require("progress-bar-webpack-plugin");
@@ -7,7 +14,10 @@ const Webpack = require("webpack");
 module.exports = {
   webpack: {
     alias: {
+      "@": path.resolve(__dirname, "./src"),
       "@components": path.resolve(__dirname, "./src/components"),
+      "@page": path.resolve(__dirname, "./src/page"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
     },
     plugins: [
       //显示启动和打包进度条
@@ -38,6 +48,7 @@ module.exports = {
           },
         },
         //加入css modules后 css名字将会转变成hash唯一值   引入就需要使用css modules（className={styles.XX})
+        // 如不支持 modules 可把如下代码屏蔽 但是引入的css类名可能会重名，导致样式出错 (className="xxx")
         cssLoaderOptions: {
           modules: {
             localIdentName: "[local][name]_[hash:base64:5]",
@@ -46,13 +57,13 @@ module.exports = {
       },
     },
   ],
-  //反向代理
   devServer: {
     host: "0.0.0.0", //启动主机号，默认是localhost,设置 0.0.0.0 服务器外部可访问
     port: 3000, //启动端口，默认是3000
     historyApiFallback: true, //解决开启页面后白屏问题
     compress: true, //一切服务都启用gzip 压缩
     hot: true, //热更新
+    //反向代理 => 前端解决跨域的问题
     proxy: {
       //请求时映射反向代理
       "/api/server": {
--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react-scripts" />
 
-
+// 在webpack里面配置 引入module的less 需要在这里导出一下
 declare module "*.less" {
   const less: any;
   export default less;
